561e349c60839b96b6bae1859abe7d468143ddab
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-09-24 11:08:54 +0200

    initial commit

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,8 @@
-import express from 'express'
+import express, {Application, Request, Response, NextFunction} from 'express'
 
-const app: express.Application = express();
+const app: Application = express();
 
-app.get('/', (req, res) => {
+app.get('/', (req:Request, res:Response, next: NextFunction) => {
     res.send('all is okay');
 });
 
