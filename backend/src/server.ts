acb6a910ae498ba2bb26048917ba276e4c7c13a7
MURANGWA Pacifique <pacifiquemurangwa001@gmail.com>
2024-06-15 23:33:42 +0200

    re-init: no longer googlemeetclone -> react-auth starter

--- backend/src/server.ts
+++
@@ -1,9 +0,0 @@
-import express, {Application, Request, Response, NextFunction} from 'express'
-
-const app: Application = express();
-
-app.get('/', (req:Request, res:Response, next: NextFunction) => {
-    res.send('all is okay');
-});
-
-app.listen(3000, () => console.log('server is in DEV mode'));
